690e2807ccc1f4ccf655058369fc0244db2a3204
YDEAX41 <yoshi_degent@hotmail.com>
2015-09-24 14:49:56 +0200

    Add dependency to pom with dom

--- src/main/java/com/realdolmen/jxp010/ex/dom/AddDependencyToPom.java
+++ src/main/java/com/realdolmen/jxp010/ex/dom/AddDependencyToPom.java
@@ -9,17 +9,12 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.IOException;
-import java.util.List;
 
-/**
- * Created by YDEAX41 on 24/09/2015.
- */
 public class AddDependencyToPom {
     public static void main(String[] args)
     {
